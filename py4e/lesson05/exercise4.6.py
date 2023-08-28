3e54fa54fbb5ff9e911b44e78626d56c26594119
Eugene Shpak <greatstillness@gmail.com>
2023-08-28 17:37:53 +0300

    Lesson5

---
+++ py4e/lesson05/exercise4.6.py
@@ -0,0 +1,10 @@
+def computepay(h, r):
+    if h > 40:
+        return r * (40 + (h - 40) * 1.5)
+    else:
+        return h * r
+
+hours = float(input("Enter Hours:"))
+rate = float(input("Enter Rate:"))
+p = computepay(hours, rate)
+print("Pay", p)
